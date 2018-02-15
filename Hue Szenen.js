f0d8cb526adcaaae6158a806812face2ab8f39c5
Christian Ernst <cernst1980@gmail.com>
2018-02-15 11:21:28 +0100

    Update Hue Szenen.js

--- Hue Szenen.js
+++ Hue Szenen.js
@@ -1,70 +1,126 @@
 var HueApi = require("node-hue-api").HueApi;
 
+// Replace IP and username!!!
 var host = "192.168.x.x",
     username = "xxxxxxxxxx",
     api = new HueApi(host, username);
     
-var groups = [],
-    objects = [];
+var groups_ = [],
+    lights_ = [],
+    objects_ = [];
 
+// Log JSON results
 var displayResults = function(result) {
     console.log(JSON.stringify(result, null, 2));
 };
 
+// Parse Light Group 0 (All Lights)
+var parseGroup0 = function(result) {
+    if (!result[i].lights){continue;} // Empty group
+    
+    var id = result.id,
+    lights = result.lights,
+    name = "All Lights";
+    console.debug('group: '+name+' id: '+id);
+    groups_[lights] = name;
+};
+
+// Parse Light Groups
 var parseGroups = function(result) {
+    for (var i = 0; i < result.length; i++) {
+        if (!result[i].lights){continue;} // Empty group
+        
+        var id = result[i].id,
+        lights = result[i].lights,
+        name = result[i].name;
+        console.debug('group: '+name+' id: '+id);
+        groups_[lights] = name;
+    }
+};
+
+// Parse Lights
+var parseLights = function(result) {
     for (var i = 0; i < result.length; i++) {
         var id = result[i].id,
         name = result[i].name;
-        console.debug('group: '+name+' id: '+id);
-        groups[id] = name;
-    }
-    groups[0] = 'All lights';
+        lights_[id] = name;
+    } 
 };
 
+// Create States in ioBroker
 var createStates = function(result) {
+    // Resync button
     createState('Hue_Scenes.Resync', false, {role: "button", name: 'Resync Groups and Scenes'});
 
-    for (var i = 0; i < result.length; i++) { 
-        var regex = /(\w{5})(?:_r|\s+:\w{2})(\d{2})(?:_d)?(\d{2,3})/;
-        if (!regex.test(result[i].appdata.data)){continue;}
+    for (var i = 0; i < result.length; i++) {
+        if (!result[i].appdata.data){continue;} // skip internal szenes
 
-        var dataid = regex.exec(result[i].appdata.data)[1],
-        room = regex.exec(result[i].appdata.data)[2],
-        number = regex.exec(result[i].appdata.data)[3],
-        id = result[i].id,
+        var id = result[i].id,
         lights = result[i].lights,
         name = result[i].name.replace(/"/g,''),
-        pathname = name.replace(/ /g,'_'),
-        group = groups[parseInt(room)] || lights;
+        pathname = name.replace(/ /g,'_');
         
-        if (!objects[room+'§'+pathname]){
+        // Get light names
+        var light_names = [];
+        for (var j = 0; j < lights.length; j++) {
+            var light_name = lights_[lights[j]];
+            light_names.push(light_name);
+        }
+
+        // Room, group or lights linked with scene
+        var group = groups_[lights] || light_names.join(", ");
+        
+        // Create States and skip duplicates
+        if (!objects_[lights+pathname]){
             console.debug('scene: '+name);
             createState('Hue_Scenes.'+pathname+'.'+id, false, {role: "button", name: 'Scene: '+name+' ('+group+')'});
-            objects[room+'§'+pathname] = true;
+            objects_[lights+pathname] = true;
         }
     }
 };
 
+// Delete States
 function deleteStates(){
+    console.debug('Deleting current scene objects...');
+    objects_ = [];
     $('javascript.0.Hue_Scenes.*').each(function (id) {
         deleteState(id);
     });
 }
 
-api.groups(function(err, groups) {
-    if (err) throw err;
-    console.debug('Processing groups...');
-    //displayResults(groups);
-    parseGroups(groups);
-});
+// Fetch data from Hue API
+function init(){
+    api.getGroup(0, function(err, group0) {
+        if (err) throw err;
+        console.debug('Processing group 0...');
+        //displayResults(group0);
+        parseGroup0(group0);
+    });
+    api.groups(function(err, groups) {
+        if (err) throw err;
+        console.debug('Processing groups...');
+        //displayResults(groups);
+        parseGroups(groups);
+    });
 
-api.scenes(function(err, scenes) {
-    if (err) throw err;
-    console.debug('Processing scenes...');
-    //displayResults(scenes);
-    createStates(scenes);
-});
+    api.lights(function(err, lights) {
+        if (err) throw err;
+        //displayResults(lights);
+        parseLights(lights.lights);
+    });
 
+    api.scenes(function(err, scenes) {
+        if (err) throw err;
+        console.debug('Processing scenes...');
+        //displayResults(scenes);
+        createStates(scenes);
+    });
+}
+
+// Init on start
+init();
+
+// Activate scene
 on({id: /^javascript\.0\.Hue_Scenes\./, val: true}, function (obj) {
     sceneId = obj.id.split('.').pop();
     console.debug('Activating '+obj.name);
@@ -75,23 +131,11 @@ on({id: /^javascript\.0\.Hue_Scenes\./, val: true}, function (obj) {
     setState(obj.id, false);
 });
 
+// Resync
 on({id: 'javascript.0.Hue_Scenes.Resync', val: true}, function (obj) {
     console.debug('Resync triggered...');
-    
-    groups = [];
-    api.groups(function(err, groups) {
-        if (err) throw err;
-        console.debug('Processing groups...');
-        parseGroups(groups);
-    });
-    
-    console.debug('Deleting current scenes...');
-    objects = [];
+    groups_ = [];
+    lights_ = [];
     deleteStates();
-    
-    api.scenes(function(err, scenes) {
-        if (err) throw err;
-        console.debug('Processing scenes...');
-        createStates(scenes);
-    });
+    init();
 });
